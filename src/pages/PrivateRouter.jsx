804aab63f874d6c86174c66ba1d12ab405e910d7
UmmuhanKavas <ummuhankavas@gmail.com>
2022-11-03 21:14:43 +0100

    paths added

--- src/pages/PrivateRouter.jsx
+++ src/pages/PrivateRouter.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
-}
+  //? Aslinda bu bilgi Global state'den okunacak
+  const isAuthenticated = false;
 
-export default PrivateRouter
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRouter;
